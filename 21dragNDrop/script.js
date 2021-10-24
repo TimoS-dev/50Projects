fd7da1abad0200eb5b48284f578c31cffb700ad0
Timo <Timo@tuhh>
2021-10-24 15:03:58 +0200

    finished dragNDrop with some help of the tutorial

--- 21dragNDrop/script.js
+++ 21dragNDrop/script.js
@@ -1,11 +1,42 @@
-let btn = document.querySelectorAll(".box");
+let fill = document.querySelector(".fill");
+let empties = document.querySelectorAll(".empty");
 
-btn.forEach((button) => {
-  button.addEventListener("mousedown", () => {
-    button.classList.add("hold");
-    console.log("hi");
-  });
-  button.addEventListener("mouseup", () => {
-    button.classList.remove("hold");
-  });
-});
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
+}
+function dragEnd() {
+  this.className = "fill";
+  console.log("hey");
+}
+function dragOver(e) {
+  console.log("over");
+  e.preventDefault();
+  /*   empty.style.background = "rgb(" + 51 + "," + 51 + "," + 51 + ")";
+  empty.style.border = "dotted 2px white"; */
+}
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += " hovered";
+}
+function dragLeave() {
+  this.className = "empty";
+}
+function dragDrop(e) {
+  e.preventDefault();
+  this.className = "empty";
+  this.append(fill);
+}
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+fill.addEventListener("dragover", dragOver);
+
+for (let empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
