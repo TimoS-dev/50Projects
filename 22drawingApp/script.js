55a0eabf0b11d819301e33ef56f5b69ffff7ffd7
Timo <timo.schneider@chainstep.com>
2021-12-20 13:11:44 +0100

    finished 22drawingApp

--- 22drawingApp/script.js
+++ 22drawingApp/script.js
@@ -1,24 +1,69 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const decreaseBtn = document.getElementById("decrease");
+const increaseBtn = document.getElementById("increase");
+let size = parseInt(document.getElementById("size").innerText);
+let color = "black";
+let isPressed = false;
+let x;
+let y;
 
-ctx.fillStyle = "green";
+function clearing() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+function colorChange() {
+  color = document.getElementById("color").value;
+}
+function decreasing() {
+  if (size <= 5) {
+    return;
+  }
+  document.getElementById("size").innerText = size - 5;
+  size = size - 5;
+}
+function increasing() {
+  if (size >= 40) {
+    return;
+  }
+  document.getElementById("size").innerText = size + 5;
+  size = size + 5;
+}
 
-function draw(radius, positionX, positionY) {
+function drawCircle(x, y) {
   ctx.beginPath();
-  var x = positionX; // x coordinate
-  var y = positionY; // y coordinate
-  var radiusSize = radius; // Arc radius
-  var startAngle = 0; // Starting point on circle
-  var endAngle = 2 * Math.PI; // End point on circle
-  var counterclockwise = true; // clockwise or counterclockwise
-
-  ctx.arc(x, y, radiusSize, startAngle, endAngle, counterclockwise);
-
+  ctx.arc(x, y, size, 0, 2 * Math.PI, true);
+  ctx.fillStyle = color;
   ctx.fill();
 }
 
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidt = size * 2;
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+  drawCircle(x, y);
+});
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+});
 canvas.addEventListener("mousemove", (e) => {
-  positionX = e.pageX;
-  positionY = e.pageY;
-  draw(10, positionX, positionY);
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
 });
